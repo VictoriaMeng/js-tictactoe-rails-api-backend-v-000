65b2b159ac4d0fece368c83ec3a878922647f8f9
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 05:15:28 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def create
     @game = Game.create(state: params[:state])
-    render json: @game, status: 201
+    render json: GameSerializer.serialize(@game), status: 201
   end
 
 end
