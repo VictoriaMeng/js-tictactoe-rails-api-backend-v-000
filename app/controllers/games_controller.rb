ebcf0b0ada73af58136a96afc430685b6bbf996d
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 05:09:28 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def create
     binding.pry
-    @game = Game.create(game_params)
+    @game = Game.create(state: params[:state])
     render json: @game, status: 201
   end
 
