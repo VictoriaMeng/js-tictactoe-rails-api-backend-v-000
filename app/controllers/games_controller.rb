e6853a9270043f21401c8b53e48ba77e13c51259
Victoria Meng <nights1ststar@protonmail.com>
2018-09-12 05:07:22 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,11 @@
 class GamesController < ApplicationController
   def create
+    @game = Game.create(game_params)
+  end
+
+  private 
+
+  def game_params
+    params.require(:game).permit(:state)
   end
 end
